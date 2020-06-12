7ac4dcaf221107018d1fcefe49ff0179709a8b65
srz_zumix <zumix.cpp@gmail.com>
2020-06-12 21:35:11 +0900

    test

--- tests/mtime-compare.sh
+++ tests/mtime-compare.sh
@@ -3,14 +3,25 @@
 DIR_A=$1
 DIR_B=$2
 
+export RESULT=0
+
+function onerror() {
+    echo $1
+    date -r $1
+    date -r ${DIR_B}/$1
+    RESULT = 1
+    export RESULT
+}
+
 cd ${DIR_A}
 for file in `\find . -type f`; do
     if [ $file -nt ${DIR_B}/${file} ]; then
         echo $file
-        exit 1
     fi
     if [ $file -ot ${DIR_B}/${file} ]; then
         echo $file
-        exit 1
     fi
 done
+cd -
+
+exit ${RESULT}
